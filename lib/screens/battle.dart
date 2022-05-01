579f2126cbcd15d9cc4dc7f07400eae376db4b56
Carrie Barrett <me.carriebarrett@gmail.com>
2022-05-01 14:07:04 -0700

    returned to old formatting of battle.dart

--- lib/screens/battle.dart
+++ lib/screens/battle.dart
@@ -9,66 +9,59 @@ class BattleScreen extends StatefulWidget {
 }
 
 class _BattleScreenState extends State<BattleScreen> {
+
   final _formKey = GlobalKey<FormState>();
 
   Future<void> showQuestion(BuildContext context) async {
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          final TextEditingController _textEditingController =
-              TextEditingController();
-          return AlertDialog(
-            content: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 5, 5, 0),
-                      child: Container(
-                        alignment: FractionalOffset.topRight,
-                        child: GestureDetector(
-                          child: const Icon(Icons.close_rounded),
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      '2 + 3 = ?',
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-                    ),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      controller: _textEditingController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Answer may not be empty';
-                        }
-                        return null;
-                      },
-                      decoration:
-                          const InputDecoration(hintText: "Enter your answer"),
-                    ),
-                  ],
-                )),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('Enter'),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    Navigator.of(context).pop();
-                  }
-                },
-              ),
-            ],
-          );
-        });
+    return await showDialog(context: context, builder: (context) {
+      final TextEditingController _textEditingController = TextEditingController();
+      return AlertDialog(
+        content: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 5, 0),
+                  child: Container(
+                    alignment: FractionalOffset.topRight,
+                    child: GestureDetector(child: const Icon(Icons.close_rounded),
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                const Text('2 + 3 = ?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                ),
+                const SizedBox(height: 5),
+                TextFormField(
+                  controller: _textEditingController,
+                  validator: (value){
+                    if (value == null || value.isEmpty) {
+                      return 'Answer may not be empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(hintText: "Enter your answer"),
+                ),
+              ],
+            )),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Enter'),
+            onPressed: (){
+              if(_formKey.currentState!.validate()) {
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ],
+      );
+    });
   }
 
   @override
@@ -77,12 +70,11 @@ class _BattleScreenState extends State<BattleScreen> {
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: TextButton(
-            onPressed: () async {
-              await showQuestion(context);
-            },
-            child: Text(
-              'Show Question',
-            )),
+          onPressed: () async {
+            await showQuestion(context);
+          },
+          child: Text('Show Question',)
+        ),
       ),
     );
   }
