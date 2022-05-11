c11d5c50f2e53fb05a2e88d2a7dcf10d89082d83
landreja <67026533+landreja@users.noreply.github.com>
2022-05-11 00:33:54 -0400

    Add Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app.dart';
-// import 'db/db_manager.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp();
   // await DBManager.initialize();
   runApp(MyApp());
 }
