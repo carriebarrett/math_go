2045045712f4b4d28761647d578a517f738d7ed6
landreja <67026533+landreja@users.noreply.github.com>
2022-05-22 15:45:51 -0400

    Update login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:math_go/screens/map_view.dart';
+import 'screens/map_view.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
