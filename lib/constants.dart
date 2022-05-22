692c3bb737239eaff9e9e3d563500e4a10a76d77
Carrie Barrett <me.carriebarrett@gmail.com>
2022-05-22 11:43:51 -0700

    updated sample beastie in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,8 +11,8 @@ const logoColor = Color.fromRGBO(255, 99, 71, 1);
 
 final sampleBeastie = Beastie(
     beastieID: 1,
-    question: '1+1',
-    answer: '2',
-    imagePath: 'assets/images/beasties/blob1.png',
     name: 'blob1',
-    type: 'math');
+    imagePath: './assets/images/beasties/leaf7.png',
+    type: 'Blob',
+    question: '2 + 2',
+    answer: 4);
