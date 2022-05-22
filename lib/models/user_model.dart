3ad7fde2aaae02bfb051c694f438c550ed942523
landreja <67026533+landreja@users.noreply.github.com>
2022-05-22 15:45:17 -0400

    updated model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,20 +1,17 @@
 class User {
-  final int userID;
+  final String userID;
   final String email;
-  final String password;
-  final int collectionID;
+  final String beastieCollectionID;
 
   User(
       {required this.userID,
       required this.email,
-      required this.password,
-      required this.collectionID});
+      required this.beastieCollectionID});
 
   factory User.fromMap(Map<dynamic, dynamic> data) {
     return User(
         userID: data['userID'] ?? 01,
         email: data['email'] ?? 'email.mail',
-        password: data['password'] ?? 'password',
-        collectionID: data['collectionID'] ?? 01);
+        beastieCollectionID: data['collectionID'] ?? 01);
   }
 }
